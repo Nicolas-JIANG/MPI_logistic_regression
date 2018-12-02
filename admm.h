b3bebc12a1146c55bd5a31aff977c4689a77897d
wessle <suttlewes@gmail.com>
2018-12-02 12:53:18 -0500

    First commit

---
+++ admm.h
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <cmath>
+#include <dlib/optimization.h>
+#include <dlib/global_optimization.h>
+
+using namespace std;
+using namespace dlib;
+
+typedef matrix<double, 0, 1> column_vector;
+
+double loss(const column_vector& beta,
+            const matrix<double>& X,
+            const matrix<double>& y,
+            int N) {
+
+    double val = -(1/(double)N)*(trans(trans(X)*y)*beta);
+
+    column_vector vec = X*beta;
+    for (int i=0; i<N; i++) {
+        val += (1/(double)N)*log(1 + exp(vec(i)));
+    }
+
+    return val;
+}
+
+column_vector prob(const column_vector& beta,
+            const matrix<double>& X,
+            const matrix<double>& y,
+            int N) {
+    
+    matrix<double> probs(N,1);
+    double prod;
+
+    for (int i=0; i<N; i++) {
+        prod = rowm(X,i)*beta;
+        if (isnan(exp(-1*prod))) {
+            probs(i) = 1;
+        }
+        else {
+            probs(i) = 1/(1+exp(-1*prod));
+        }
+    }
+
+    return probs;
+}
+
+column_vector d_loss(const column_vector& beta,
+                     const matrix<double>& X,
+                     const matrix<double>& y,
+                     int N) {
+
+    matrix<double> p(N,1);
+    p = prob(beta, X, y, N);
+
+    return (1/(double)N)*trans(X)*(p - y);
+}
+
+double beta_iter_obj_func(const column_vector& beta,
+                          const column_vector& z,
+                          const column_vector& u,
+                          const matrix<double>& X,
+                          const matrix<double>& y,
+                          int N,
+                          double rho) {
+
+    column_vector temp = beta - z + u;
+    
+    return loss(beta, X, y, N) + (rho/2)*(trans(temp)*temp);
+}
+
+/* functor for use as objective function in beta iterations */
+struct beta_iter_obj_func_parametrized {
+  public:
+    beta_iter_obj_func_parametrized(const column_vector& z,
+                                    const column_vector& u,
+                                    const matrix<double>& X,
+                                    const matrix<double>& y,
+                                    int N,
+                                    double rho) : 
+        z(z), u(u), X(X), y(y), N(N), rho(rho) {}
+
+    double operator()(const column_vector& beta) const {
+        return beta_iter_obj_func(beta, z, u, X, y, N, rho);
+    }
+
+  private:
+    column_vector z, u;
+    matrix<double> X, y;
+    int N;
+    double rho;
+};
+
+column_vector beta_iter_obj_func_gradient(const column_vector& beta,
+                                          const column_vector& z,
+                                          const column_vector& u,
+                                          const matrix<double>& X,
+                                          const matrix<double>& y,
+                                          int N,
+                                          double rho) {
+
+    return d_loss(beta, X, y, N) + rho*(beta - z + u);
+}
+
+/* functor for use as gradient in beta iterations */
+struct beta_iter_obj_func_gradient_parametrized {
+  public:
+    beta_iter_obj_func_gradient_parametrized(const column_vector& z,
+                                             const column_vector& u,
+                                             const matrix<double>& X,
+                                             const matrix<double>& y,
+                                             int N,
+                                             double rho) :
+        z(z), u(u), X(X), y(y), N(N), rho(rho) {}
+
+    column_vector operator()(const column_vector& beta) const {
+        return beta_iter_obj_func_gradient(beta, z, u, X, y, N, rho);
+    }
+
+  private:
+    column_vector z, u;
+    matrix<double> X, y;
+    int N;
+    double rho;
+};
+
+/* z update functions */
+double soft_threshold(double x, double c) {
+    if (x > c) {
+        return x-c;
+    }
+    else if (abs(x) <= c) {
+        return 0;
+    }
+    else {
+        return x+c;
+    }
+}
+
+column_vector z_iter(const column_vector& beta_av,
+                     const column_vector& u_av,
+                     int p,                             /* # of predictors */
+                     double lambda,
+                     double rho,
+                     int num_pes) {
+    column_vector z_new;
+    z_new = zeros_matrix<double>(p+1,1);
+    for (int i=0; i<p+1; i++) {
+        if (i == 0) {
+            z_new(i) = beta_av(i) + u_av(i);
+        }
+        else {
+            z_new(i) = soft_threshold(beta_av(i) + u_av(i),
+                                      lambda/(rho*num_pes));
+        }
+    }
+
+    return z_new;
+}
+
+/* u update */
+column_vector u_iter(const column_vector& beta_curr,    /* current beta */
+                     const column_vector& z_curr,       /* current z */
+                     const column_vector& u_prev) {     /* previous u */
+    return u_prev + beta_curr - z_curr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* end */
---
+++ mpi_admm.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <string>
+#include <mpi.h>
+#include "admm.h"
+
+using namespace std;
+
+/* utility functions for use with MPI_Reduce and MPI_Bcast */
+void vector_to_array(const column_vector&, double*, int);
+void array_to_vector(column_vector&, const double*, int);
+
+int
+main(int argc, char *argv[])
+{
+    int N = atoi(argv[1]);              /* dimension of EACH data chunk */
+    int p = atoi(argv[2]);
+    int iters = atoi(argv[3]);
+    double rho = atof(argv[4]);
+    double lambda = atof(argv[5]);
+
+    int rank, num_pes;
+    double local_beta[p+1], global_beta[p+1];
+    double global_z[p+1], local_u[p+1], global_u[p+1];
+
+/* 
+ * Begin parallel program
+ */
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_pes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    /* non-root ranks */
+    if (rank != 0) {
+        /* read rank-specific data chunk into X and y */
+        string filename = "data_";
+        filename += to_string(rank);
+        filename += ".txt";
+        ifstream file(filename);
+
+        matrix<double> X(N,p+1);
+        matrix<double> y(N,1);
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<p+1; j++) {
+                file >> X(i,j);
+            }
+            file >> y(i);
+        }
+        file.close();
+
+        /* carry out iterations */
+        column_vector beta, z, u;
+        beta = z = u = zeros_matrix<double>(p+1,1);
+
+        for (int i=0; i<iters; i++) {
+            cout << "Rank " << rank << ": iteration " << i+1 << endl;
+
+            /* beta update */
+            beta_iter_obj_func_parametrized f(z, u, X, y, N, rho);
+            beta_iter_obj_func_gradient_parametrized df(z, u, X, y, N, rho);
+
+            find_min(lbfgs_search_strategy(10),
+                     objective_delta_stop_strategy(1e-7).be_verbose(),
+                     f, df, beta, -1);
+
+            /* z update -- send beta to rank 0, then receive z from rank 0 */
+            vector_to_array(beta, local_beta, p+1);
+            MPI_Reduce(&local_beta, &global_beta, p+1, MPI_DOUBLE, MPI_SUM,
+                       0, MPI_COMM_WORLD);
+            MPI_Bcast(&global_z, p+1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            array_to_vector(z, global_z, p+1);
+
+            /* u update -- update u using z, send u to rank 0 */
+            u = u_iter(beta, z, u);
+            vector_to_array(u, local_u, p+1);
+            MPI_Reduce(&local_u, &global_u, p+1, MPI_DOUBLE, MPI_SUM,
+                       0, MPI_COMM_WORLD);
+        }
+    }
+
+    /* rank 0 */
+    else {
+            column_vector beta_av, u_av, z;
+            beta_av = u_av = z = zeros_matrix<double>(p+1,1);
+
+            /* NOTE: we MUST set local_beta and local_u to 0 to avoid
+             * undefined behavior in MPI_Reduce */
+            vector_to_array(beta_av, local_beta, p+1);
+            vector_to_array(u_av, local_u, p+1);
+
+       for (int i=0; i<iters; i++) {
+            /* gather beta, compute z, and broadcast z update */
+            MPI_Reduce(&local_beta, &global_beta, p+1, MPI_DOUBLE, MPI_SUM,
+                       0, MPI_COMM_WORLD);
+            array_to_vector(beta_av, global_beta, p+1);
+            beta_av = (1/(double)(num_pes-1))*beta_av;
+            z = z_iter(beta_av, u_av, p, lambda, rho, num_pes);
+            vector_to_array(z, global_z, p+1);
+            MPI_Bcast(&global_z, p+1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+            /* gather u and calculate u_av */
+            MPI_Reduce(&local_u, &global_u, p+1, MPI_DOUBLE, MPI_SUM,
+                       0, MPI_COMM_WORLD);
+            array_to_vector(u_av, global_u, p+1);
+            u_av = (1/(double)(num_pes-1))*u_av;
+        }
+
+        cout << "Beta:\n" << beta_av << endl;
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
+
+void vector_to_array(const column_vector& v, double* arr, int length) {
+    for (int i=0; i<length; i++) {
+        arr[i] = v(i);
+    }
+}
+
+void array_to_vector(column_vector& v, const double* arr, int length) {
+    for (int i=0; i<length; i++) {
+        v(i) = arr[i];
+    }
+}
